16f4a7378e2348a311f15d0baa937c3061d80320
s-ws-go <aplord@naver.com>
2021-12-27 18:42:03 +0900

    01. WebSocket Setting

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,19 @@
-alert("hi");
+// 여기는 프론트 엔드
+
+// 서버와 브라우저 간의 연결(헷갈리지 말것)
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener('open', () => {
+  console.log('Connected to the Server! OOO');
+});
+socket.addEventListener('message', (message) => {
+  console.log(message);
+  console.log(`Data from Server : ${message.data}`);
+});
+socket.addEventListener('close', () => {
+  console.log('DisConnected to the Server! XXX');
+});
+
+setTimeout(() => {
+  socket.send('Hello from Front!');
+}, 5000);
--- src/server.js
+++ src/server.js
@@ -1,12 +1,34 @@
-import express from "express";
+//여기는 백엔드
+
+import http from 'http';
+import WebSocket from 'ws';
+
+import express from 'express';
 
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (req, res) => res.render('home'));
+app.get('/*', (req, res) => res.redirect('/'));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// 서버 안에서 http 와 wss 서버간의 연결
+wss.on('connection', (socket) => {
+  //wss 서버가 ㅇ니라, socket에 있는 메서드 사용 가능하다.
+  console.log('..Connect to the Browser');
+  socket.on('close', () => {
+    console.log('..DisConnect from the Browser');
+  });
+  socket.on('message', (message) => {
+    console.log(`This Is Front-Message : ${message}`);
+  });
+  socket.send('Hello from Server to the Browser!!');
+});
+
+server.listen(3000, handleListen);
