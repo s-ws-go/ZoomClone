dd8e5e348bb94d5ee851f4c8a45ff6582b7bfbdf
s-ws-go <aplord@naver.com>
2021-12-27 20:02:46 +0900

    #03 Nickname part One

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,28 +1,43 @@
 // 여기는 프론트 엔드
-
 // 서버와 브라우저 간의 연결(헷갈리지 말것)
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 const messageUl = document.querySelector('ul');
-const messageForm = document.querySelector('form');
+const messageForm = document.querySelector('#msgForm');
+const nickForm = document.querySelector('#nickForm');
 
 socket.addEventListener('open', () => {
   console.log('Connected to the Server! OOO');
 });
 socket.addEventListener('message', (message) => {
-  console.log(message);
-  console.log(`Data from Server : ${message.data}`);
+  const li = document.createElement('li');
+  messageUl.append(li);
+  li.innerText = message.data;
 });
 socket.addEventListener('close', () => {
   console.log('DisConnected to the Server! XXX');
 });
 
-messageForm.addEventListener('submit', (e) => {
+function makeMessage(type, payload) {
+  return { type, payload };
+}
+
+const handleMessage = (e) => {
   e.preventDefault();
   const Input = messageForm.querySelector('input');
-  socket.send(Input.value);
+  socket.send(JSON.stringify(makeMessage('new_message', Input.value)));
   Input.value = '';
-});
+};
+
+const handleNick = (e) => {
+  e.preventDefault();
+  const Input = nickForm.querySelector('input');
+  socket.send(JSON.stringify(makeMessage('nickname', Input.value)));
+  Input.value = '';
+};
+
+messageForm.addEventListener('submit', handleMessage);
+nickForm.addEventListener('submit', handleNick);
 
 // setTimeout(() => {
 //   socket.send('Hello from Front!');
--- src/server.js
+++ src/server.js
@@ -34,3 +34,8 @@ wss.on('connection', (socket) => {
 });
 
 server.listen(3000, handleListen);
+
+// 닉네임을 입력할 수 있는 form 또한 만들어 준다
+// submit 때 보내줄 message가 채팅메세지인지 닉네임인지 구분되지 않은채로 프론트 -> 서버로 전달된다
+// type와 payload를 각각 nickname, message (타입) 설정하고 payload에 값을 대응하면 되는데,
+// send 메서드는 string만을 보낼 수 있다. --> Json으로 바꿔서 전송토록 해 보자.
